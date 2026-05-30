15a5d06de31b2e61b80e79d344de2c2b1c4b9b7f
Paul Corbineau <paulcorbineau@gmail.com>
2026-05-30 04:00:22 +0200

    build: integrate ccache for faster incremental builds

---
+++ cmake/ccache.cmake
@@ -0,0 +1,14 @@
+if(NOT USE_CCACHE)
+	message(STATUS "Not using ccache")
+	return()
+endif()
+
+find_program(CCACHE_PROGRAM ccache)
+if(NOT CCACHE_PROGRAM)
+	message(STATUS "ccache not found, not using ccache")
+	return()
+endif()
+
+message(STATUS "Using ccache: ${CCACHE_PROGRAM}")
+set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
+set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE_PROGRAM})
